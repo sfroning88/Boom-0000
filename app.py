884552e10168b223c612816af02d6418d084a478
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-30 12:19:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, sys
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, requests, jsonify
 
 # create a Flask app
 app = Flask(__name__)
@@ -10,10 +10,19 @@ def home():
     return render_template('chat.html')
 
 # generic button function
-@app.route('/BUTTON_FUNCTION', methods=['POST'])
-def generate_outbound_call():
+@app.route('/BUTTON_FUNCTION_ONE', methods=['POST'])
+def BUTTON_FUNCTION_ONE():
     try:
-        return jsonify({'success': True, 'message': 'Button success.'}), 200
+        return jsonify({'success': True, 'message': 'Button Function One success.'}), 200
+    
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)}), 400
+
+# generic button function
+@app.route('/BUTTON_FUNCTION_TWO', methods=['POST'])
+def BUTTON_FUNCTION_TWO():
+    try:
+        return jsonify({'success': True, 'message': 'Button Function Two success.'}), 200
     
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)}), 400
@@ -24,4 +33,4 @@ if __name__ == '__main__':
         sys.exit(1)
 
     # run the app
-    app.run()
+    app.run(debug=True)
